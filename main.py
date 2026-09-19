5bfd62c81d975b5f4ef3ffd28f880a3282c295c5
Karanvir Thethy <karanvirthethy24@gmail.com>
2026-09-18 21:23:35 -0400

    Add basic station lookup

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("StepFreeTO")
+stations = {
+
+    "A": ["B", "C"],
+    "B": ["A", "D"],
+    "C": ["A", "D"],
+    "D": ["B", "C", "E"],
+    "E": ["D"],
+}
+
+
+start_station = input("Please enter your station: ").upper()
+
+if start_station in stations:
+    print(stations.get(start_station))
+else: 
+    print('Please choose a correct option: ')
+
